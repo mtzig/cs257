18bffcf9c3d024a033439535b9c73e4615e56671
Thomas Zeng <68117495+mtzig@users.noreply.github.com>
2021-11-23 18:45:42 +0000

    more json change

--- webapp/static/language-template.js
+++ webapp/static/language-template.js
@@ -39,7 +39,7 @@ function loadLanguageInfo() {
     let endangermentListBody = '';
     let locationListBody = '';
 
-    languages = JSON.parse(languages,(key, value) =>
+    languages = JSON.parse(JSON.stringify(languages),(key, value) =>
     value == null
     ? value = 'n/a' // set value to 'n/a' if it was null
     : value);
